034529f131995869c9075c1afe79bfb5ea08d498
Ben Dalling <b.dalling@cbdq.io>
2024-05-25 08:58:02 +0100

    fix: dev: Add Delta Spark tests to the pipeline.

--- examples/delta/delta.py
+++ examples/delta/delta.py
@@ -6,3 +6,9 @@ builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
 
 spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+data = spark.range(0, 5)
+data.write.format("delta").save("/tmp/delta-table")
+
+df = spark.read.format("delta").load("/tmp/delta-table")
+df.show()
