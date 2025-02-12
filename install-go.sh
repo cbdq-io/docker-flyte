8dafe659ffe5622a3023dd043a4be9608213c49e
Ben Dalling <b.dalling@cbdq.io>
2025-02-12 14:33:19 +0000

    fix(chore): Correct download URL for the x86_64 arch.

--- install-go.sh
+++ install-go.sh
@@ -3,11 +3,14 @@
 # Install the go language from binary.
 #############################################################################
 GO_VERSION=1.23.6
+HARDWARE_PLATFORM=$( uname -m )
 
 case "$( uname -m )" in
   aarch64) URL="https://go.dev/dl/go${GO_VERSION}.linux-arm64.tar.gz" ;;
+  x86_64)  URL="https://go.dev/dl/go1.24.0.linux-amd64.tar.gz" ;;
   *)       URL="https://go.dev/dl/go${GO_VERSION}.linux-$( uname -m ).tar.gz" ;;
 esac
 
-wget -O /tmp/go.tgz "$URL"
+echo "INFO: Using ${URL} for the ${HARDWARE_PLATFORM} platform."
+wget -qO /tmp/go.tgz "$URL"
 tar -C /usr/local -xzf /tmp/go.tgz
